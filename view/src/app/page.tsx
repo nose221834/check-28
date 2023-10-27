53560171c1d094d638b446ccecc9801733ffffd9
nose221834 <takumi.ichinose@fuller.co.jp>
2023-10-27 15:59:57 +0900

    continue

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -5,33 +5,34 @@ import Form from './components/form';
 import weeks from './weeks'
 
 
-interface Props{
-  en:number;
-  ja:string;
-  time:string;
-}
-
-
 export default function Home() {
 
 
   const [dayTimes,setDayTimes] = useState([
-  "00.00",
-  "00.00",
-  "00.00",
-  "00.00",
-  "00.00",
+  "00:00",
+  "00:00",
+  "00:00",
+  "00:00",
+  "00:00",
   ])
 
+  const [sumTime,setSumTimes] = useState(0)
 
-  function handleTime(e: React.ChangeEvent<HTMLInputElement>) {
-    const nextDayInfo = dayTimes.map((day,i) => {
+
+  function handleTime(index: number,e: React.ChangeEvent<HTMLInputElement>) {
+    const nextDayInfo = dayTimes.map((daytime,i) => {
       if (i === index) {
-      } return e.target.value
+        return e.target.value;
+      } else {
+        return daytime;
+      }
     })
     setDayTimes(nextDayInfo)
   }
 
+  function handleSumTime() {
+
+  }
 
   return (
     <form action="" method="get" className="form-example" >
@@ -39,11 +40,8 @@ export default function Home() {
         <h2>勤務時間を入力してください</h2>
       {dayTimes.map((dayTime,i) =>
       <>
-        <Form dayEn={weeks[0].en} dayJa={weeks[0].ja} dayTime={dayTime} onchange={() => handleTime()} key={weeks[0].en}/>
-        <Form dayEn={weeks[1].en} dayJa={weeks[1].ja} dayTime={dayTime} onchange={handleTime} key={weeks[1].en}/>
-        <Form dayEn={weeks[2].en} dayJa={weeks[2].ja} dayTime={dayTime} onchange={handleTime} key={weeks[2].en}/>
-        <Form dayEn={weeks[3].en} dayJa={weeks[3].ja} dayTime={dayTime} onchange={handleTime} key={weeks[3].en}/>
-        <Form dayEn={weeks[4].en} dayJa={weeks[4].ja} dayTime={dayTime} onchange={handleTime} key={weeks[4].en}/>
+        <Form dayEn={weeks[i].en} dayJa={weeks[i].ja} dayTime={dayTime} onchange={(e) => handleTime(i,e)} key={i}/>
+        <div key={weeks[i].ja}>勤務時間：{dayTime}</div>
       </>
       )}
       <button type="submit">Submit form</button>
